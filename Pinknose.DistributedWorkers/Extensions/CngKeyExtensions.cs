a4c387b18677c1ea50002dc5eef596c5b07f748f
Mease, Cameron <cameron@pinknose.net>
2020-05-18 11:54:47 -0400

    Added extension to CngKey for creating string representations of the public key and hashes of the key.

---
+++ Pinknose.DistributedWorkers/Extensions/CngKeyExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pinknose.DistributedWorkers.Extensions
+{
+    public static class CngKeyExtensions
+    {
+        public static string GetPublicKeyHash(this CngKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            byte[] keyBytes = key.Export(CngKeyBlobFormat.EccPublicBlob);
+            using SHA256Managed hasher = new SHA256Managed();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte singleByte in hasher.ComputeHash(keyBytes))
+            {
+                sb.Append(singleByte.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string PublicKeyToString(this CngKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            byte[] keyBytes = key.Export(CngKeyBlobFormat.EccFullPublicBlob);
+
+            return Convert.ToBase64String(keyBytes);
+        }
+    }
+}
