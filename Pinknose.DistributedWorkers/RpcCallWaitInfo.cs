91d0bfb99b0c9f5c8c5488feb0c838290ce51d78
Mease, Cameron <cameron@pinknose.net>
2020-05-18 11:59:14 -0400

    Added class to store information about an in-progress RPC call.

---
+++ Pinknose.DistributedWorkers/RpcCallWaitInfo.cs
@@ -0,0 +1,17 @@
+﻿using Pinknose.DistributedWorkers.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Pinknose.DistributedWorkers
+{
+    public class RpcCallWaitInfo
+    {
+        public RpcCallResult CallResult { get; set; }
+
+        internal EventWaitHandle WaitHandle { get; set;  }
+
+        public MessageBase ResponseMessage { get; set; }
+    }
+}
