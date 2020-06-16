b89cdb2c05873af48152ff676159b5f7e09b033c
Mease, Cameron <cameron@pinknose.net>
2020-06-16 16:39:56 -0400

    Adding extension to Random for getting Uint32 values.

---
+++ Pinknose.DistributedWorkers/Extensions/RandomExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Pinknose.DistributedWorkers.Extensions
+{
+    public static class RandomExtensions
+    {
+        public static UInt32 NextUInt32(this Random random)
+        {
+            var bytes = new byte[4];
+
+            random.NextBytes(bytes);
+
+            return BitConverter.ToUInt32(bytes);
+        }
+    }
+}
