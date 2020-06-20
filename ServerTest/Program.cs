ff7735747002e4a1e9dc363b4411fb0ee77c2a0c
Mease, Cameron <cameron@pinknose.net>
2020-06-20 11:51:47 -0400

    * Added JSON configuration file for the ServerTest app.

--- ServerTest/Program.cs
+++ ServerTest/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Pinknose.DistributedWorkers.Clients;
+using Pinknose.DistributedWorkers.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ServerTest
 {
@@ -6,7 +11,28 @@ namespace ServerTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Assuming first argument is the JSON Configuration file name
+            JObject config = JObject.Parse(File.ReadAllText(args[0]));
+
+            var serverIdentity = MessageClientInfo.Import(config["ServerIdentity"].ToString(), "monkey123");
+
+            var clientsJArray = (JArray)config["ClientIdentities"];
+            List<MessageClientInfo> clients = new List<MessageClientInfo>();
+
+            foreach (var item in clientsJArray)
+            {
+                clients.Add(MessageClientInfo.Import(item.ToString()));
+            }
+
+            var server = new MessageServerConfigurationBuilder()
+                .RabbitMQCredentials(config["RabbitMQServer"]["UserName"].Value<string>(), config["RabbitMQServer"]["Password"].Value<string>())
+                .RabbitMQServerHostName(config["RabbitMQServer"]["HostName"].Value<string>())
+                .ServerInfo(serverIdentity)
+                .AddClientInfoRange(clients)
+                .AutoDeleteQueuesOnClose(true)
+                .QueuesAreDurable(false)
+                .CreateMessageServer();
+
         }
     }
 }
